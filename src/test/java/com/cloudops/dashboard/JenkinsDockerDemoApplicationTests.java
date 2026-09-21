1836165ce2adc2e0b15d6c69b46867a45c188946
Bouthik KR <bouthik4649@gmail.com>
2026-09-21 18:48:25 +0530

    Initial commit - CloudOps monitoring dashboard

---
+++ src/test/java/com/cloudops/dashboard/JenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudops.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
